394a958141dedf0c3bcc845df4bc2b24fc4c29b7
YHCM <a2646399721@163.com>
2024-05-19 16:27:32 +0800

    完成主界面布局

--- src/downloader/Main.java
+++ src/downloader/Main.java
@@ -2,10 +2,8 @@ package downloader;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -15,19 +13,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("URL下载器");
 
-        // 根据屏幕自适应大小
-        double width = 800;
-        double height = 500;
+        // 窗口大小不可改变
+        double width = 985;
+        double height = 535;
+        primaryStage.setResizable(false);
 
-        try {
-            Rectangle2D bounds = Screen.getScreens().get(0).getBounds();
-
-            width = bounds.getWidth() / 1.6;
-            height = bounds.getHeight() / 1.6;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        // 设置 fxml 文件
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/downloader/home.fxml")));
         Scene scene = new Scene(root, width, height);
 
